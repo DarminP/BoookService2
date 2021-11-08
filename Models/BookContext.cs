1c46310ea109960221fb2b15b92e2de0647eddea
Darmin Poturovic <d.poturovic@gmail.com>
2021-11-08 10:33:35 +0100

    Add project files.

---
+++ Models/BookContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+
+namespace BoookService2.Models
+{
+    public class BookContext :DbContext
+    {
+        public DbSet<Book> Books { get; set; }
+
+        public BookContext(DbContextOptions<BookContext> options) : base(options)
+        {
+
+        }
+
+
+        //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        //{
+        //    optionsBuilder.UseSqlServer(@"Server=DESKTOP-VAD6TT8;Database=BookDB;Trusted_Connection=True;");
+        //}
+    }
+}
